f90ec231b5e8e4ecf1e889d554fbb00f49ea56f7
thao <angelasnow2004@gmail.com>
2025-08-27 22:56:37 +0700

    bt3

--- demo/src/main/java/docfile/Main.java
+++ demo/src/main/java/docfile/Main.java
@@ -1,29 +1,42 @@
 package docfile;
 
 import java.io.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.io.File;
-import java.io.FilterInputStream;
 
 public class Main {
     public static void main(String[] args) {
-        try (FileInputStream file = new FileInputStream("D:\\taofile\\txt.txt")){
-            try ( BufferedReader br = new BufferedReader(new InputStreamReader(file)))
-            {
+        try (FileInputStream file = new FileInputStream("D:\\taofile\\txt.txt")) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(file))) {
                 String line;
+
+                
                 line = br.readLine();
-                while ((line = br.readLine()) != null){
+
+                while ((line = br.readLine()) != null) {
                     String[] arr = line.split(",");
-                    System.out.println("so cot:" + arr.length);
-                    if (arr.length<5){
-                         System.out.println("ko du thong tin");
-                        }
-                    else {
+
+                    System.out.println("Số cột: " + arr.length);
+
+                    if (arr.length < 5) {
+                        System.out.println("Không đủ thông tin!");
+                    } else {
                         String ten = arr[1].trim();
-                        String mavn = arr[4].trim();
-                        System.out.println(ten +" "+ mavn);
+                        String maNV = arr[4].trim();
+                        String tentep = ten + "_" + maNV;
+
+                        System.out.println(tentep);
+
+                        File rootFolder = new File("D:\\taofile\\" + tentep);
+
+                        if (!rootFolder.exists()) {
+                            boolean created = rootFolder.mkdirs(); 
+                            if (created) {
+                                 rootFolder.getAbsolutePath();
+                            } else {
+                                System.out.println("loi ko tao duoc");
+                            }
+                        } else {
+                            System.out.println(" Thư mục đã tồn tại.");
+                        }
                     }
                 }
             }
